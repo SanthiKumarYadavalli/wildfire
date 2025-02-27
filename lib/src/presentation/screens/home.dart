6eb6efaa8e4d9c7843c6d9f305ac7e235e112175
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-02-27 17:27:53 +0530

    feat: add calendar slider and a button to select a specific date

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -4,48 +4,92 @@ import 'package:go_router/go_router.dart';
 import 'package:wildfire/src/providers/auth_provider.dart';
 import 'package:wildfire/src/providers/habit_provider.dart';
 import 'package:wildfire/src/providers/user_provider.dart';
+import 'package:calendar_slider/calendar_slider.dart';
 
 class HomeScreen extends ConsumerWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
   
+  final _calcontroller = CalendarSliderController();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(currUserProvider);
     final habits = ref.watch(userHabitsProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Wildfire"),
-        actions: [
-          MenuAnchor(
-            menuChildren: [
-              MenuItemButton(
-                onPressed: () => ref.read(authProvider.notifier).logout(),
-                leadingIcon: Icon(Icons.logout),
-                child: Text("Logout"),
-              )
-            ],
-            builder: (context, controller, child) {
-              return user.when(
-                data: (data) {
-                  return IconButton(
-                    icon: CircleAvatar(
-                      backgroundImage: NetworkImage(data!.profileImageUrl),
-                    ),
-                    onPressed: () {
-                      if (controller.isOpen) {
-                        controller.close();
-                      } else {
-                        controller.open();
-                      }
-                    }
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(161), 
+        child: Column(
+          children: [
+            AppBar(
+              title: Text("Wildfire"),
+              centerTitle: true,
+              leading: IconButton(
+                icon: Icon(Icons.calendar_month_rounded),
+                onPressed: () async {
+                  final picked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1947, 8, 15),
+                    lastDate: DateTime.now(),
                   );
-                },
-                loading: () => CircularProgressIndicator(),
-                error: (error, stackTrace) => Icon(Icons.error),
-              );
-            },
-          )
-        ],
+                  if (picked != null) {
+                    _calcontroller.goToDay(picked);
+                  }
+                }
+              ),
+              actions: [
+                TextButton(
+                  child: Text("Today"),
+                  onPressed: () {
+                    _calcontroller.goToDay(DateTime.now());
+                  },
+                ),
+                MenuAnchor(
+                  menuChildren: [
+                    MenuItemButton(
+                      onPressed: () => ref.read(authProvider.notifier).logout(),
+                      leadingIcon: Icon(Icons.logout),
+                      child: Text("Logout"),
+                    )
+                  ],
+                  builder: (context, controller, child) {
+                    return user.when(
+                      data: (data) {
+                        return IconButton(
+                          icon: CircleAvatar(
+                            backgroundImage: NetworkImage(data!.profileImageUrl),
+                          ),
+                          onPressed: () {
+                            if (controller.isOpen) {
+                              controller.close();
+                            } else {
+                              controller.open();
+                            }
+                          }
+                        );
+                      },
+                      loading: () => CircularProgressIndicator(),
+                      error: (error, stackTrace) => Icon(Icons.error),
+                    );
+                  },
+                )
+              ],
+            ),
+            CalendarSlider(
+              controller: _calcontroller,
+              fullCalendar: false,
+              initialDate: DateTime.now(),
+              firstDate: DateTime.parse("1947-08-15"),
+              lastDate: DateTime.now(),
+              selectedTileBackgroundColor: Theme.of(context).colorScheme.primary,
+              selectedDateColor: Theme.of(context).colorScheme.onPrimary,
+              tileBackgroundColor: Theme.of(context).colorScheme.surface,
+              dateColor: Theme.of(context).colorScheme.onSurface,
+              onDateSelected: (date) {
+                print(date);
+              },
+            ),
+          ]
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
