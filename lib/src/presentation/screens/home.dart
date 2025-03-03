3ace2462c8cefdcc8a121873be8c3501ac1e6313
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-03-03 20:45:11 +0530

    feat: pull to refresh for habits

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -40,52 +40,55 @@ class HomeScreen extends ConsumerWidget {
             if (data.isEmpty) {
               return Text("Add a new habit!");
             }
-            return ListView.builder(
-              padding: EdgeInsets.only(bottom: 80),
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                final formattedDate = DateFormat("yyyy-MM-dd").format(currDate);
-                final isCompleted = data[index].dates.containsKey(formattedDate);
-                return Container(
-                  padding: EdgeInsets.all(10),
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromRGBO(149, 157, 165, 0.2),
-                        blurRadius: 24,
-                        spreadRadius: 0,
-                        offset: Offset(0, 8),
-                      ),   
-                    ],
-                    border: Border(
-                      left: BorderSide(
-                        color: (isCompleted) ? Colors.deepOrange : Colors.grey,
-                        width: 5,
+            return RefreshIndicator(
+              onRefresh: () => ref.refresh(userHabitsProvider.future),
+              child: ListView.builder(
+                padding: EdgeInsets.only(bottom: 80),
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  final formattedDate = DateFormat("yyyy-MM-dd").format(currDate);
+                  final isCompleted = data[index].dates.containsKey(formattedDate);
+                  return Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(149, 157, 165, 0.2),
+                          blurRadius: 24,
+                          spreadRadius: 0,
+                          offset: Offset(0, 8),
+                        ),   
+                      ],
+                      border: Border(
+                        left: BorderSide(
+                          color: (isCompleted) ? Colors.deepOrange : Colors.grey,
+                          width: 5,
+                        ),
+                      ),
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        data[index].title,
+                        style: TextStyle(
+                          fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
+                          fontWeight: Theme.of(context).textTheme.titleMedium!.fontWeight,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: (isCompleted) 
+                              ? Icon(Icons.local_fire_department_sharp, color: Colors.deepOrange, size: 40) 
+                              : Icon(Icons.local_fire_department_outlined, color: Colors.grey, size: 40),
+                        onPressed: () {
+                          ref.read(userHabitsProvider.notifier).toggleCompletion(data[index].id, formattedDate);
+                        },
                       ),
                     ),
-                    color: Theme.of(context).cardColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      data[index].title,
-                      style: TextStyle(
-                        fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
-                        fontWeight: Theme.of(context).textTheme.titleMedium!.fontWeight,
-                      ),
-                    ),
-                    trailing: IconButton(
-                      icon: (isCompleted) 
-                            ? Icon(Icons.local_fire_department_sharp, color: Colors.deepOrange, size: 40) 
-                            : Icon(Icons.local_fire_department_outlined, color: Colors.grey, size: 40),
-                      onPressed: () {
-                        ref.read(userHabitsProvider.notifier).toggleCompletion(data[index].id, formattedDate);
-                      },
-                    ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             );
           },
           loading: () => CircularProgressIndicator(),
