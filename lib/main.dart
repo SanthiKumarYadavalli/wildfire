3429539de213e2b6edd5a21600f20fef077ec0d4
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-03-14 13:45:53 +0530

    feat: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wildfire/src/app/routes/app_router.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(ProviderScope(child: MyApp()));
 }
 
--- lib/src/app/routes/app_router.dart
+++ lib/src/app/routes/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import "package:go_router/go_router.dart";
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -12,6 +13,9 @@ part 'app_router.g.dart';
 @riverpod
 GoRouter router(ref) {
   final AsyncValue<String> auth = ref.watch(loginProvider);
+  auth.whenData((data) {
+    FlutterNativeSplash.remove();
+  });
   return GoRouter(routes: [
     GoRoute(
       path: "/login",
